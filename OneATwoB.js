2f44db38de4050e153a9446c41348d1f63890b34
sharkfoolish <sharkfoolish@chsh.chc.edu.tw>
2022-04-24 15:29:11 +0800

    add error message

--- OneATwoB.js
+++ OneATwoB.js
@@ -14,21 +14,25 @@ const  OneATwoB = {
     },
     
     judgeAnswer : function(inputAnswer){
-        var a = 0;
-        var b = 0;
-        for (var i=0; i<4; i++) {
-            var index = this.answer.indexOf(inputAnswer[i]);
-            if (index == i) {
-                a++;
-            } else if(index!=-1){
-                b++;
+        if(inputAnswer.length!=4 || !(isInt(inputAnswer-0))) {
+            error.innerHTML = '<div class="alert alert-danger" role="alert">輸入錯誤，只能輸入每位不能重複的四位數</div>';
+        } else {
+            var a = 0;
+            var b = 0;
+            for (var i=0; i<4; i++) {
+                var index = this.answer.indexOf(inputAnswer[i]);
+                if (index == i) {
+                    a++;
+                } else if(index!=-1){
+                    b++;
+                }
             }
+            this.number += 1;
+            if (a == 4) {
+                alert('恭喜您用了' + this.number + '次，答對了最終的答案' + inputAnswer);
+            }
+            this.rederRecord(inputAnswer, a+'A'+b+'B');
         }
-        this.number += 1;
-        if (a == 4) {
-            alert('恭喜您用了' + this.number + '次，答對了最終的答案' + inputAnswer);
-        }
-        this.rederRecord(inputAnswer, a+'A'+b+'B');
     },
 
     rederRecord : function(inputAnswer, result){
